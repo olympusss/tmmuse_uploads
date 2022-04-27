0735451e73e1a6c00be14ab28a9c870997215c7e
olympusss <rasulov.titu@gmail.com>
2022-04-27 11:13:47 +0000

    commit

--- tmmuse_galleries.sql
+++ tmmuse_galleries.sql
@@ -16,11 +16,62 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: galleries; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.galleries (
+    id integer NOT NULL,
+    medium_image character varying,
+    large_image character varying,
+    profile_id integer,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+ALTER TABLE public.galleries OWNER TO postgres;
+
+--
+-- Name: galleries_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.galleries_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.galleries_id_seq OWNER TO postgres;
+
+--
+-- Name: galleries_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.galleries_id_seq OWNED BY public.galleries.id;
+
+
+--
+-- Name: galleries id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.galleries ALTER COLUMN id SET DEFAULT nextval('public.galleries_id_seq'::regclass);
+
+
 --
 -- Data for Name: galleries; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.galleries (id, medium_image, large_image, profile_id, created_at, updated_at) VALUES (153, '/uploads/galleries/medium_images/65bf249a-9e26-4f70-a6c6-e19a9bbd37dc.jpg', '/uploads/galleries/large_images/5d146984-0e15-4e36-a703-8073bc82df0e.jpg', 278, '2022-04-24 13:42:14.728663', '2022-04-24 13:42:14.728674');
+COPY public.galleries (id, medium_image, large_image, profile_id, created_at, updated_at) FROM stdin;
+153	/uploads/galleries/medium_images/65bf249a-9e26-4f70-a6c6-e19a9bbd37dc.jpg	/uploads/galleries/large_images/5d146984-0e15-4e36-a703-8073bc82df0e.jpg	278	2022-04-24 13:42:14.728663	2022-04-24 13:42:14.728674
+\.
 
 
 --
@@ -28,6 +79,29 @@ INSERT INTO public.galleries (id, medium_image, large_image, profile_id, created
 --
 
 SELECT pg_catalog.setval('public.galleries_id_seq', 153, true);
+
+
+--
+-- Name: galleries galleries_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.galleries
+    ADD CONSTRAINT galleries_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: ix_galleries_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX ix_galleries_id ON public.galleries USING btree (id);
+
+
+--
+-- Name: galleries galleries_profile_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.galleries
+    ADD CONSTRAINT galleries_profile_id_fkey FOREIGN KEY (profile_id) REFERENCES public.profiles(id);
 
 
 --
